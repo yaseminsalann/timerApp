60cf132fdfdbee05b387ff3447442b8cc3a472d7
yaseminsalann <yaseminsalann@gmail.com>
2025-01-26 17:45:24 +0300

    TİMER
    timer

---
+++ TimerApp/TimerAppTests/TimerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppTests.swift
+//  TimerAppTests
+//
+//  Created by Yasemin salan on 26.01.2025.
+//
+
+import Testing
+@testable import TimerApp
+
+struct TimerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
